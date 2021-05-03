d521550b2ec2e105622d5d27f8546c95dd4598d9
usama samoo <usamasamoo786@gmail.com>
2021-05-04 01:15:32 +0500

    project done

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+ 
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+
+// Scroll Top 
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 500 ) {
+        $(".askew-navbar").addClass("fixed-header");
+    } else{
+        $(".askew-navbar").removeClass("fixed-header");
+    }
+
+   
+
+});
+
